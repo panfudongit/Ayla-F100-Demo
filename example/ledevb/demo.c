8394f55d4a5e489102bb379208ca1922195ac8b5
dong <116865375@qq.com>
2016-06-30 12:59:36 +0800

    1 del useless date 2 add power set to host 3 add temp set to host

--- example/ledevb/demo.c
+++ example/ledevb/demo.c
@@ -92,10 +92,8 @@ const char version[] = VERSION;
 #endif /* DEMO_IMG_MGMT || AYLA_BUILD_VERSION */
 
 static u8 factory_reset;
+#define COM3 3
 
-
-static void set_input(struct prop *, void *arg, void *valp, size_t len);
-static void set_cmd(struct prop *, void *arg, void *valp, size_t len);
 static void set_dec_in(struct prop *prop, void *arg, void *valp, size_t len);
 
 #ifdef DEMO_IMG_MGMT
@@ -108,11 +106,9 @@ int send_template_version(struct prop *, void *arg);
 void template_version_sent(void);
 #endif
 
-static s32 input;
 static s32 output;
 static s32 decimal_in;
 static s32 decimal_out;
-static u8 blue_button;
 static char cmd_buf[TLV_MAX_STR_LEN + 1];
 
 #ifdef DEMO_SCHED_LIB
@@ -123,24 +119,6 @@ static struct sched_prop schedule_in;
 static void set_schedule_in(struct prop *prop, void *arg, void *valp,
     size_t len);
 #endif
-
-
-static void set_led(struct prop *prop, void *arg, void *valp, size_t len)
-{
-	u8 val = *(u8 *)valp;
-	u32 pin = (u32)arg;
-
-	stm32_set_led(pin, val);
-}
-
-static int send_led(struct prop *prop, void *arg)
-{
-	u8 val;
-	u32 pin = (u32)prop->arg;
-
-	val = stm32_get_led(pin);
-	return prop_send(prop, &val, sizeof(val), arg);
-}
 
 static int send_version(struct prop *prop, void *arg)
 {
@@ -155,9 +133,108 @@ static int send_schedule(struct prop *prop, void *arg)
 #endif
 
 
+static s32 devices1_power;
+static s32 devices2_power;
+static s32 devices3_power;
+static s32 devices4_power;
+static s32 devices5_power;
+static s32 devices6_power;
+static s32 devices7_power;
+static s32 devices8_power;
+static s32 devices9_power;
+static s32 devices10_power;
+static s32 devices11_power;
+static s32 devices12_power;
+static s32 devices13_power;
+static s32 devices14_power;
+static s32 devices15_power;
+static s32 devices16_power;
+static s32 devices1_temp;
+static s32 devices2_temp;
+static s32 devices3_temp;
+static s32 devices4_temp;
+static s32 devices5_temp;
+static s32 devices6_temp;
+static s32 devices7_temp;
+static s32 devices8_temp;
+static s32 devices9_temp;
+static s32 devices10_temp;
+static s32 devices11_temp;
+static s32 devices12_temp;
+static s32 devices13_temp;
+static s32 devices14_temp;
+static s32 devices15_temp;
+static s32 devices16_temp;
+static void set_devices_send_data(char data, char type, char devid, int len);
+static void set_devices_power(struct prop *prop, void *arg, void *valp, size_t len);
+static void set_devices_temp(struct prop *prop, void *arg, void *valp, size_t len);
 struct prop prop_table[] = {
-	{ "Blue_button", ATLV_BOOL, NULL, prop_send_generic,
-	    &blue_button, sizeof(blue_button), AFMT_READ_ONLY},
+#define DEMO_VERSION       0
+	{ "version",          ATLV_UTF8, NULL,               send_version,      NULL,           0,                 AFMT_READ_ONLY},
+#define DEVICES1_POWER     1
+	{ "devices1_work_power",     ATLV_INT,  set_devices_power,      prop_send_generic, &devices1_power,     sizeof(devices1_power)},
+#define DEVICES2_POWER     2
+	{ "devices2_work_power",     ATLV_INT,  set_devices_power,      prop_send_generic, &devices2_power,     sizeof(devices2_power)},
+#define DEVICES3_POWER     3
+	{ "devices3_work_power",     ATLV_INT,  set_devices_power,      prop_send_generic, &devices3_power,     sizeof(devices3_power)},
+#define DEVICES4_POWER     4
+	{ "devices4_work_power",     ATLV_INT,  set_devices_power,      prop_send_generic, &devices4_power,     sizeof(devices4_power)},
+#define DEVICES5_POWER     5
+	{ "devices5_work_power",     ATLV_INT,  set_devices_power,      prop_send_generic, &devices5_power,     sizeof(devices5_power)},
+#define DEVICES6_POWER     6
+	{ "devices6_work_power",     ATLV_INT,  set_devices_power,      prop_send_generic, &devices6_power,     sizeof(devices6_power)},
+#define DEVICES7_POWER     7
+	{ "devices7_work_power",     ATLV_INT,  set_devices_power,      prop_send_generic, &devices7_power,     sizeof(devices7_power)},
+#define DEVICES8_POWER     8
+	{ "devices8_work_power",     ATLV_INT,  set_devices_power,      prop_send_generic, &devices8_power,     sizeof(devices8_power)},
+#define DEVICES9_POWER     9
+	{ "devices9_work_power",     ATLV_INT,  set_devices_power,      prop_send_generic, &devices9_power,     sizeof(devices9_power)},
+#define DEVICES10_POWER    10
+	{ "devices10_work_power",    ATLV_INT,  set_devices_power,     prop_send_generic, &devices10_power,    sizeof(devices10_power)},
+#define DEVICES11_POWER    11
+	{ "devices11_work_power",    ATLV_INT,  set_devices_power,     prop_send_generic, &devices11_power,    sizeof(devices11_power)},
+#define DEVICES12_POWER    12
+	{ "devices12_work_power",    ATLV_INT,  set_devices_power,     prop_send_generic, &devices12_power,    sizeof(devices12_power)},
+#define DEVICES13_POWER    13
+	{ "devices13_work_power",    ATLV_INT,  set_devices_power,     prop_send_generic, &devices13_power,    sizeof(devices13_power)},
+#define DEVICES14_POWER    14
+	{ "devices14_work_power",    ATLV_INT,  set_devices_power,     prop_send_generic, &devices14_power,    sizeof(devices14_power)},
+#define DEVICES15_POWER    15
+	{ "devices15_work_power",    ATLV_INT,  set_devices_power,     prop_send_generic, &devices15_power,    sizeof(devices15_power)},
+#define DEVICES16_POWER    16
+	{ "devices16_work_power",    ATLV_INT,  set_devices_power,     prop_send_generic, &devices16_power,    sizeof(devices16_power)},
+#define DEVICES1_TEMP    17
+	{ "devices1_work_temp",     ATLV_INT,  set_devices_temp,      prop_send_generic, &devices1_temp,     sizeof(devices1_temp)},
+#define DEVICES2_TEMP    18
+	{ "devices2_work_temp",     ATLV_INT,  set_devices_temp,      prop_send_generic, &devices2_temp,     sizeof(devices2_temp)},
+#define DEVICES3_TEMP    19
+	{ "devices3_work_temp",     ATLV_INT,  set_devices_temp,      prop_send_generic, &devices3_temp,     sizeof(devices3_temp)},
+#define DEVICES4_TEMP    20
+	{ "devices4_work_temp",     ATLV_INT,  set_devices_temp,      prop_send_generic, &devices4_temp,     sizeof(devices4_temp)},
+#define DEVICES5_TEMP    21
+	{ "devices5_work_temp",     ATLV_INT,  set_devices_temp,      prop_send_generic, &devices5_temp,     sizeof(devices5_temp)},
+#define DEVICES6_TEMP    22
+	{ "devices6_work_temp",     ATLV_INT,  set_devices_temp,      prop_send_generic, &devices6_temp,     sizeof(devices6_temp)},
+#define DEVICES7_TEMP    23
+	{ "devices7_work_temp",     ATLV_INT,  set_devices_temp,      prop_send_generic, &devices7_temp,     sizeof(devices7_temp)},
+#define DEVICES8_TEMP    24
+	{ "devices8_work_temp",     ATLV_INT,  set_devices_temp,      prop_send_generic, &devices8_temp,     sizeof(devices8_temp)},
+#define DEVICES9_TEMP    25
+	{ "devices9_work_temp",     ATLV_INT,  set_devices_temp,      prop_send_generic, &devices9_temp,     sizeof(devices9_temp)},
+#define DEVICES10_TEMP   26
+	{ "devices10_work_temp",    ATLV_INT,  set_devices_temp,     prop_send_generic, &devices10_temp,    sizeof(devices10_temp)},
+#define DEVICES11_TEMP   27
+	{ "devices11_work_temp",    ATLV_INT,  set_devices_temp,     prop_send_generic, &devices11_temp,    sizeof(devices11_temp)},
+#define DEVICES12_TEMP   28
+	{ "devices12_work_temp",    ATLV_INT,  set_devices_temp,     prop_send_generic, &devices12_temp,    sizeof(devices12_temp)},
+#define DEVICES13_TEMP   29
+	{ "devices13_work_temp",    ATLV_INT,  set_devices_temp,     prop_send_generic, &devices13_temp,    sizeof(devices13_temp)},
+#define DEVICES14_TEMP   30
+	{ "devices14_work_temp",    ATLV_INT,  set_devices_temp,     prop_send_generic, &devices14_temp,    sizeof(devices14_temp)},
+#define DEVICES15_TEMP   31
+	{ "devices15_work_temp",    ATLV_INT,  set_devices_temp,     prop_send_generic, &devices15_temp,    sizeof(devices15_temp)},
+#define DEVICES16_TEMP   32
+	{ "devices16_work_temp",    ATLV_INT,  set_devices_temp,     prop_send_generic, &devices16_temp,    sizeof(devices16_temp)},
 #define PROP_BUTTON 0
 	{ "output", ATLV_INT, NULL, prop_send_generic, &output,
 	    sizeof(output), AFMT_READ_ONLY},
@@ -170,20 +247,13 @@ struct prop prop_table[] = {
 #define PROP_DEC_OUT 3
 	{ "decimal_in", ATLV_CENTS, set_dec_in, prop_send_generic,
 	    &decimal_in, sizeof(decimal_in)},
-	{ "Blue_LED", ATLV_BOOL, set_led, send_led,
-	    (void *)(1 << LED0_PIN), 1},
-	{ "Green_LED", ATLV_BOOL, set_led, send_led,
-	    (void *)(1 << LED1_PIN), 1},
 #ifdef DEMO_SCHED_LIB
 	{ "schedule_in", ATLV_SCHED, set_schedule_in, NULL, &schedule_in},
 	{ "schedule_out", ATLV_SCHED, NULL, send_schedule, NULL, 0,
 	  AFMT_READ_ONLY},
 #define PROP_SCHED_OUT 8
 #endif
-	{ "cmd", ATLV_UTF8, set_cmd, prop_send_generic, &cmd_buf[0]},
-	{ "input", ATLV_INT, set_input, prop_send_generic,
-	    &input, sizeof(input)},
-	{ "version", ATLV_UTF8, NULL, send_version, NULL, 0, AFMT_READ_ONLY},
+
 #ifdef DEMO_IMG_MGMT
 	{ "inactive_version", ATLV_UTF8, NULL, send_inactive_version, NULL,
 	  0, AFMT_READ_ONLY },
@@ -209,21 +279,75 @@ struct prop prop_table[] = {
 	{ NULL }
 };
 u8 prop_count = (sizeof(prop_table) / sizeof(prop_table[0])) - 1;
-
-static void set_input(struct prop *prop, void *arg, void *valp, size_t len)
+#define NAME_LEN 21
+#define CMD_TYPE_POWER 0xF0
+#define CMD_TYPE_TEMP  0xF1
+#define CMD_TYPE_RES   0xF2
+#define CMD_TYPE_REQ   0xF3
+static void set_devices_send_data(char data, char type, char devid, int len)
 {
-	s32 i = *(s32 *)valp;
+	char pack[8];
 
-	if (len != sizeof(s32)) {
-		return;
+	if(type == (char)CMD_TYPE_POWER) // dev power
+	{
+		if(data > 0x02) //data: 0x00, 0x01, 0x02
+			return;
 	}
-	input = i;
-	if (i > 0x7fff || i < -0x8000) {
-		output = -1;		/* square would overflow */
-	} else {
-		output = i * i;
+	if(type == (char)CMD_TYPE_TEMP) //set dev temperature
+	{
+		if(data < 0x5 || data > 0x23) // set temperature val: 0x5  ~ 0x23
+			return;
 	}
-	prop_table[PROP_OUTPUT].send_mask = valid_dest_mask;
+	pack[0] = 0x55;  //direction to host
+	pack[1] = 0x04;  //pack len
+	pack[2] = devid; //dev id
+	pack[3] = type;  //cmd type
+	pack[4] = data;  //set val
+	pack[5] = pack[2] ^ pack[3] ^ pack[4]; //verify
+	pack[6] = 0xED;  //end
+
+	USART_send_buf(pack, 7, COM3);
+}
+static void set_devices_power(struct prop *prop, void *arg, void *valp, size_t len)
+{
+	char dev_name[NAME_LEN];
+	char data = *(char *)valp;
+	int dev_name_len = prop->name_len;
+	u8 dev_id = 0;
+
+	memcpy(dev_name, prop->name, dev_name_len);
+	if(dev_name_len == 19) //devices1_work_power ~ devices9_work_power
+	{
+		dev_id = dev_name[7] - 48;
+	}
+	if(dev_name_len == 20) //devices10_work_power ~ devices16_work_power
+	{
+		dev_id = 10 + dev_name[8] - 48;
+	}
+
+	set_devices_send_data(data, CMD_TYPE_POWER, dev_id, 7);
+
+	return;
+}
+
+static void set_devices_temp(struct prop *prop, void *arg, void *valp, size_t len)
+{
+	char data = *(char *)valp;
+	int dev_name_len = prop->name_len;
+	u8 dev_id = 0;
+
+	if(dev_name_len == 18) //devices1_work_temp ~ devices9_work_temp
+	{
+			dev_id = prop->name[7] - 48;
+	}
+	if(dev_name_len == 19) //devices10_work_temp ~ devices16_work_temp
+	{
+			dev_id = 10 + prop->name[8] - 48;
+	}
+
+	set_devices_send_data(data, CMD_TYPE_TEMP, dev_id, 7);
+
+	return;
 }
 
 static void set_dec_in(struct prop *prop, void *arg, void *valp, size_t len)
@@ -236,16 +360,6 @@ static void set_dec_in(struct prop *prop, void *arg, void *valp, size_t len)
 	decimal_in = i;
 	decimal_out = i;
 	prop_table[PROP_DEC_OUT].send_mask = valid_dest_mask;
-}
-
-static void set_cmd(struct prop *prop, void *arg, void *valp, size_t len)
-{
-	if (len >= sizeof(cmd_buf)) {
-		len = sizeof(cmd_buf) - 1;
-	}
-	memcpy(cmd_buf, valp, len);
-	cmd_buf[len] = '\0';
-	prop_table[PROP_LOG].send_mask = valid_dest_mask;
 }
 
 #ifdef DEMO_SCHED_LIB
@@ -271,7 +385,6 @@ static void set_schedule_in(struct prop *prop, void *arg, void *valp,
  */
 void demo_set_button_state(u8 button_value)
 {
-	blue_button = button_value;
 	prop_table[PROP_BUTTON].send_mask = valid_dest_mask;
 }
 
