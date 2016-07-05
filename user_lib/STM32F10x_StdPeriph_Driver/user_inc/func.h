27eed73f437edaf7ff42cab20d15303e69b83401
dong <116865375@qq.com>
2016-07-05 14:05:26 +0800

    1.wall code init 2. uart1, RXPA10 TXPA9 3. uart3, RXPB11 RXPB10 4. SPI1, READ_PA12_INT_PA3

--- user_lib/STM32F10x_StdPeriph_Driver/user_inc/func.h
+++ user_lib/STM32F10x_StdPeriph_Driver/user_inc/func.h
@@ -2,14 +2,14 @@
 #include "stm32f10x.h"
 #include "stm32f10x_usart.h"
 
-#define READ_PB10_INIT_PB11
-//#define READ_PA12_INT_PA3
-//#define USER_SPI1
-#define USER_SPI2
+//#define READ_PB10_INIT_PB11
+#define READ_PA12_INT_PA3
+#define USER_SPI1
+//#define USER_SPI2
 #define TRUE			1
 #define FALSE			0
 #define USER_DEBUG
-#define USER_DEBUG_COM 3
+#define USER_DEBUG_COM 1
 void printd(char *p);
 /*****************************************
 *Uart func
@@ -18,9 +18,9 @@ void USART_init(void);
 void USART_send_char(char ch, int com);
 void USART_send_buf(char *buf, int len, int com);
 char USART_receive_char(int com);
-//#define ENABLE_UART1
-//#define ENABLE_UART1_RPA10_TPA9
-#define USART1_BaudRate 9600
+#define ENABLE_UART1
+#define ENABLE_UART1_RPA10_TPA9
+#define USART1_BaudRate 4800
 #define USART1_WordLength USART_WordLength_8b
 #define USART1_StopBits USART_StopBits_1
 #define USART1_Parity USART_Parity_No
@@ -33,9 +33,9 @@ char USART_receive_char(int com);
 //#define USART2_Parity USART_Parity_No
 //#define USART2_HardwareFlowControl USART_HardwareFlowControl_None
 #define ENABLE_UART3
-#define ENABLE_UART3_RPC11_TPC10
-//#define ENABLE_UART3_RPB11_TPB10
-#define USART3_BaudRate 9600
+//#define ENABLE_UART3_RPC11_TPC10
+#define ENABLE_UART3_RPB11_TPB10
+#define USART3_BaudRate 4800
 #define USART3_WordLength USART_WordLength_8b
 #define USART3_StopBits USART_StopBits_1
 #define USART3_Parity USART_Parity_No
